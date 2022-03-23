2bd0b2596a3b00fed51b7c5f70e24ebd4ea68809
Yoga2003 <101611506+Yoga2003@users.noreply.github.com>
2022-03-23 23:51:05 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,31 +6,21 @@ using System.Threading.Tasks;
 
 namespace Taxi
 {
-    public class Taxi
+    class Program
     {
-        public string DriverName { get; set; }
-        public bool OnDuty { get; set; }
-        public int NumPassenger { get; set; }
+        static void Main(string[] args)
+        {
+            Taxi taxi = new Taxi();
 
-        private string OnDutyYN()
-        {
-            return OnDuty ? "Yes" : "No";
-        }
+            taxi.DriveName = "Yoga";
+            taxi.OnDuty = true;
+            taxi.Numpassenger = 10;
 
-        public void TaxiInfo()
-        {
-            Console.WriteLine("driver name: {0}", DriverName);
-            Console.WriteLine("on duty: {0}", OnDutyYN());
-            Console.WriteLine("number of passenger: {0}", NumPassenger);
-        }
-        public void PickupPassenger()
-        {
-            Console.WriteLine("{0} sedang menjemput penumpang", DriverName);
-        }
-        public void DropOffPassenger()
-        {
-            Console.WriteLine("{0} selesai mengantar penumpang", DriverName);
-        }
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassengger();
 
+            Console.ReadKey();
+        }
     }
 }
